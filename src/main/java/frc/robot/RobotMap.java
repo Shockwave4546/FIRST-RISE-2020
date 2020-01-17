6f80f205bc2b8c804912b717a455736ae3896c30
Sage Coff <pixelmoongaming4702@gmail.com>
2020-01-16 20:27:44 -0500

    making robotmap thing

---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,23 @@
+package frc.robot;
+
+public class RobotMap {
+    // Controllers //
+    public static final int cDriverPort = 0;
+    public static final int cCoDriverPort = 1;
+
+    // PWM //
+    public static final int mSpinPort = 0;
+    public static final double mSpinPos = 0.6;
+    public static final double mSpinNeg = 0.6;
+
+    public static final int mPivotPort = 1;
+    public static final double mPivotSpeed = 1.0;
+    public static final double mPivotPos = 1.0;
+    public static final double mPivotNeg = 1.0;
+
+    // CAN Bus // 
+
+    // Digital //
+    public static final int vLimitPort = 8;
+    public static final int hLimitPort = 9;
+}
