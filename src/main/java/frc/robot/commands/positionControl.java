ec0ba458ad54c7ab357271d229d1f9e3914d5f9d
Dilan Patel <dilanpace1496@gmail.com>
2020-01-21 10:46:24 -0500

    e

--- src/main/java/frc/robot/commands/positionControl.java
+++ src/main/java/frc/robot/commands/positionControl.java
@@ -186,6 +186,7 @@ public class positionControl extends CommandBase {
     @Override
     public void end(boolean interrupted) {
         itisFinished = false;
+        Robot.oi.mSpinner.rotateMotor(0.0);
     }
 
     // Returns true when the command should end.
