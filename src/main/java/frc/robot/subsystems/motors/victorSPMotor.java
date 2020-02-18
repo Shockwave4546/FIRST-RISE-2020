7352cee5cf586a21dabeab40ca4a77bbb7bf7aa5
Sage Coff <pixelmoongaming4702@gmail.com>
2020-02-17 19:17:57 -0500

    added ctre vendor library and victorSPX class

--- src/main/java/frc/robot/subsystems/motors/victorSPMotor.java
+++ src/main/java/frc/robot/subsystems/motors/victorSPMotor.java
@@ -6,30 +6,30 @@ import edu.wpi.first.wpilibj.Victor;
 
 
 
-public class victorMotor extends Motor{
+public class victorSPMotor extends Motor{
     private Victor kMotor;
 
 
 
     // Initialization with default positive/negative scaling
-    public victorMotor(final int port){
+    public victorSPMotor(final int port){
         super(port, 1, 1);
         kMotor = new Victor(port);
     }
     // Initialization with specified positive/negative scaling
-    public victorMotor(final int port, final double pos, final double neg){
+    public victorSPMotor(final int port, final double pos, final double neg){
         super(port, pos, neg);
         kMotor = new Victor(port);
     }
     // Initialization  with default positive/negative scaling
     // and encoder
-    public victorMotor(final int port, final int chaA, final int chaB){
+    public victorSPMotor(final int port, final int chaA, final int chaB){
         super(port, 1, 1, chaA, chaB);
         kMotor = new Victor(port);
     }
     // Initialization  with specified positive/negative scaling
     // and encoder
-    public victorMotor(final int port, final double pos, final double neg, final int chaA, final int chaB){
+    public victorSPMotor(final int port, final double pos, final double neg, final int chaA, final int chaB){
         super(port, pos, neg, chaA, chaB);
         kMotor = new Victor(port);
     }
