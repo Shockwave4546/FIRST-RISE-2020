0ba4fda2d36979a5a3c39d42d19fcaecec3e8c7c
Sage Coff <pixelmoongaming4702@gmail.com>
2020-03-08 15:28:31 -0400

    Fix to Motor Ports

--- src/main/java/frc/robot/commands/startintake.java
+++ src/main/java/frc/robot/commands/startintake.java
@@ -36,7 +36,7 @@ public class startintake extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    Robot.oi.feedMotor.rotateMotor(speedIntake);
+    Robot.oi.intakeMotor.rotateMotor(speedIntake);
   }
 
   // Called once the command ends or is interrupted.
--- src/main/java/frc/robot/commands/stopintake.java
+++ src/main/java/frc/robot/commands/stopintake.java
@@ -28,7 +28,7 @@ public class stopintake extends CommandBase {
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
-      Robot.oi.feedMotor.rotateMotor(0);
+      Robot.oi.intakeMotor.rotateMotor(0);
     }
   
     // Called once the command ends or is interrupted.
